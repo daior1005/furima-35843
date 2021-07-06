a9c61bec4d30f1fe6978c9e54d4cf6b206bade08
daior1005 <ironmands12@gmail.com>
2021-07-06 17:59:53 +0900

    テストコード修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,13 +6,13 @@ RSpec.describe User, type: :model do
 
   describe 'ユーザー新規登録' do
     it 'nicknameが空だと登録できない' do
-      user = User.new(nickname: '', email: 'kkk@gmail.com', password: '00000000', password_confirmation: '00000000')
-      user.valid?
+      @user = User.new(nickname: '', email: 'kkk@gmail.com', password: '00000000', password_confirmation: '00000000')
+      @user.valid?
       expect(user.errors.full_messages).to include("Nickname can't be blank")
     end
     it 'メールアドレスが必須' do
-      user = User.new(nickname: 'abe', email: '', password: '00000000', password_confirmation: '00000000')
-      user.valid?
+      @user = User.new(nickname: 'abe', email: '', password: '00000000', password_confirmation: '00000000')
+      @user.valid?
       expect(user.errors.full_messages).to include("Email can't be blank")
     end
 
