eb0a3ecbbf84becb8d66593478a25c251ebd1364
daior1005 <ironmands12@gmail.com>
2021-07-06 17:41:58 +0900

    コントローラーのkey変更修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
-                                      keys: [:nickname, :email, :first_name, :family_name, :first_name_kana, :family_name_kana,
+                                      keys: [:nickname, :first_name, :family_name, :first_name_kana, :family_name_kana,
                                              :birth_date])
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe User, type: :model do
     @user = Furima.build(:user)
   end
 
-
   describe 'ユーザー新規登録' do
     it 'nicknameが空だと登録できない' do
       user = User.new(nickname: '', email: 'kkk@gmail.com', password: '00000000', password_confirmation: '00000000')
@@ -16,7 +15,7 @@ RSpec.describe User, type: :model do
       user.valid?
       expect(user.errors.full_messages).to include("Email can't be blank")
     end
-    
+
     it 'メールアドレスが一意性' do
       @user.save
       another_user = Furima.build(:user)
