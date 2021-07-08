8eed5bf4dc2b41482ee73bd590d1624d418d3252
daior1005 <ironmands12@gmail.com>
2021-07-08 14:43:27 +0900

    README修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'furimas/index'
   root to: "furimas#index"
-  # resources :items, only: [:new, :create, :index, :show]
+  resources :items, only: [:index, :new]
 
 
 end
