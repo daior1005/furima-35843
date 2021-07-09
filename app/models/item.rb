97443cfd47dbb6c5efc13b043ec9f846bb8dcabf
daior1005 <ironmands12@gmail.com>
2021-07-09 16:46:56 +0900

    Activehush導入・設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,13 +3,32 @@ class Item < ApplicationRecord
   belongs_to :user
 
   validates :item_name, presence: true
+
   validates :explain, presence: true
-  validates :category_id, presence: true
-  validates :item_status_id, presence: true
-  validates :delivery_charge_id, presence: true
-  validates :delivery_area_id, presence: true
-  validates :shipment_day_id, presence: true
+
+  validates :category_id, 
+             numericality: { other_than: 1 , message: "can't be blank"}, 
+             presence: true
+
+  validates :item_status_id, 
+             numericality: { other_than: 1 , message: "can't be blank"},
+             presence: true
+
+  validates :delivery_charge_id, 
+             numericality: { other_than: 1 , message: "can't be blank"},
+             presence: true
+
+  validates :delivery_area_id, 
+             numericality: { other_than: 1 , message: "can't be blank"},
+             presence: true
+
+  validates :shipment_day_id, 
+             numericality: { other_than: 1 , message: "can't be blank"},
+             presence: true
+             
   validates :cost, presence: true
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :item_status, :delivery_charge, :delivery_area, :shipment_day
 
 end
