78a4a6996a0bdb818fca07b1bc62a533e3e70e06
daior1005 <ironmands12@gmail.com>
2021-07-09 18:28:27 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,39 +1,24 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :item_status, :delivery_charge, :delivery_area, :shipment_day, :user
   has_one :purchase
-  belongs_to :user
   has_one_attached :image
 
+  with_options presence: true do
+    validates :image
+    validates :item_name
+    validates :explain
+    validates :cost, inclusion: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ }
+  end
+  
+  with_options numericality: { other_than: 1 , message: "can't be blank"} do
+   validates :category_id 
+   validates :item_status_id
+   validates :delivery_charge_id
+   validates :delivery_area_id
+   validates :shipment_day_id
+  end
 
-  validates :item_name, 
-             presence: true
-
-  validates :explain, 
-             presence: true
-
-  validates :category_id, 
-             numericality: { other_than: 1 , message: "can't be blank"}, 
-             presence: true
-
-  validates :item_status_id, 
-             numericality: { other_than: 1 , message: "can't be blank"},
-             presence: true
-
-  validates :delivery_charge_id, 
-             numericality: { other_than: 1 , message: "can't be blank"},
-             presence: true
-
-  validates :delivery_area_id, 
-             numericality: { other_than: 1 , message: "can't be blank"},
-             presence: true
-
-  validates :shipment_day_id, 
-             numericality: { other_than: 1 , message: "can't be blank"},
-             presence: true
-
-  validates :cost, 
-             presence: true
-
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category, :item_status, :delivery_charge, :delivery_area, :shipment_day
+  validates :cost, numericality: true
 
 end
